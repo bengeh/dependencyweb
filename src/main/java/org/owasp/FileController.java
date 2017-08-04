6b03e072057f4d8a300fa31891f26d37d321121c
BenGeh <bengeh@BenGehs-MacBook-Pro.local>
2017-08-05 02:32:31 +0800

    first commit

---
+++ src/main/java/org/owasp/FileController.java
@@ -0,0 +1,68 @@
+package org.owasp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+
+@Controller
+public class FileController {
+
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    /*@GetMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }*/
+
+    @GetMapping("/")
+    public String index(){
+        return "upload";
+    }
+
+
+    @PostMapping("/upload")
+    public String fileUploadPage(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes){
+        System.out.println("inside the fileuploadpage");
+
+        if(file.isEmpty()){
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try{
+            byte[] Byte = file.getBytes();
+            System.out.println("this is the file name: " + file.getOriginalFilename());
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:uploadStatus";
+    }
+
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+
+}
